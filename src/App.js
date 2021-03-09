a363e9ded6d20e1da394e1dd793973946df7eda7
pi-gul <photon-drive@protonmail.com>
2021-03-09 12:01:18 +0100

    Create a repository with a react to-do list (YT project)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {newItem: '', list: []};
+  }
+
+  updateInput(key, value) {
+    this.setState({
+      [key]: value
+    })
+  }
+  addItem() {
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+    const list = [...this.state.list];
+    list.push(newItem);
+    this.setState({list, newItem:''});
+  }
+  deleteItem(id) {
+    const list = [...this.state.list];
+    const updatedList = list.filter((item) => {return item.id !== id});
+    this.setState({list: updatedList});
+  }
+  render() {
+    return (
+      <div className='app'>
+        <h1>MY LIST</h1>
+        <div className='list-box'>
+          <h2>Add an Item...</h2>
+          <input
+            type='text'
+            placeHolder='Type item here'
+            value={this.state.newItem}
+            onChange={(e) => {this.updateInput('newItem', e.target.value)}}
+          />
+          <button className='add-button' onClick={() => {this.addItem()}}>Add</button>
+          <ul>
+            {this.state.list.map((item) => {
+              return (
+                <li key={item.id}>
+                  {item.value}
+                  <button onClick={() => this.deleteItem(item.id)}>X</button>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
